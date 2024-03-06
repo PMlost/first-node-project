03faefe77d4c4300fab312fdc46c823c24ee206e
Praveen M <praveenmurali0123@gmail.com>
2024-03-06 13:04:56 +0530

    cleanup

--- utilities/multer.js
+++ utilities/multer.js
@@ -1,22 +1,3 @@
-// const multer = require("multer");
-// const path = require("path");
-// // console.log(path.join());
-
-// // Set up Multer storage configuration
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     const uploadFolder = "public/add-product";
-//     cb(null, uploadFolder); // Set the destination folder for uploaded files
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + "-" + file.originalname); // Set the filename to be unique
-//   },
-// });
-
-// const upload = multer({ storage: storage });
-
-// module.exports = upload;
-
 const multer = require("multer");
 
 const storage = multer.diskStorage({
